da0ef3746d53c37a299a33267cdd2f2047237ffe
Timothée Mazzucotelli <dev@pawamoy.fr>
2024-08-10 14:28:16 +0200

    feat: Implement no-op extension

--- src/griffe_sphinx/__init__.py
+++ src/griffe_sphinx/__init__.py
@@ -5,4 +5,6 @@ Parse Sphinx-comments above attributes as docstrings.
 
 from __future__ import annotations
 
-__all__: list[str] = []
+from griffe_sphinx._internals.extension import SphinxCommentsExtension
+
+__all__: list[str] = ["SphinxCommentsExtension"]
---
+++ src/griffe_sphinx/_internals/extension.py
@@ -0,0 +1,5 @@
+import griffe
+
+
+class SphinxCommentsExtension(griffe.Extension):
+    """Parse Sphinx-comments above attributes as docstrings."""
